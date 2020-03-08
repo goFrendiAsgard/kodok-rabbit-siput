ecf7bac0095ed7e49a0e1fa99b9b2f596dbc28f1
gofrendi <gofrendiasgard@gmail.com>
2020-03-08 08:15:36 +0700

    adjust with newest template from zaruba

--- services/kodok/main.js
+++ services/kodok/main.js
@@ -5,14 +5,14 @@ const bodyParser = require('body-parser');
 
 const serviceDesc = require('./serviceDesc');
 const { createApp, startApp } = require('./helpers/express/expressHelper');
-const { createConnection: createRmqConnection, sendToQueue, consume } = require('./helpers/amqp/amqpHelper');
+const { createRmq, sendToQueue } = require('./helpers/amqp/amqpHelper');
 const { logger, rmqEvent } = serviceDesc;
 
 async function main() {
     try {
         // Variables
         const app = createApp(serviceDesc, express, bodyParser);
-        const rmq = await createRmqConnection(serviceDesc.rmq, amqplib);
+        const rmq = await createRmq(serviceDesc, serviceDesc.rmq, amqplib);
 
         // Http routes
         app.all('/', (req, res) => {
@@ -21,9 +21,9 @@ async function main() {
 
         app.all('/pesan-ke-siput', (req, res) => {
             // dapatkan pesan dari user
-            const pesan = req.query['pesan'] ? req.query['pesan']: 'kosong';
+            const pesan = req.query['pesan'] ? req.query['pesan'] : 'kosong';
             // kirim pesan ke siput
-            sendToQueue(serviceDesc, rmq, 'siput', pesan);
+            sendToQueue(rmq, 'siput', pesan);
             // kasih tahu ke user bahwa kita udah kirim pesan ke siput
             res.send('Hai user, pesanmu ke siput sudah dikirim');
         });
--- services/siput/main.js
+++ services/siput/main.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 
 const serviceDesc = require('./serviceDesc');
 const { createApp, startApp } = require('./helpers/express/expressHelper');
-const { createConnection: createRmqConnection, sendToQueue, consume } = require('./helpers/amqp/amqpHelper');
+const { createRmq, sendToQueue, consume } = require('./helpers/amqp/amqpHelper');
 const { logger, rmqEvent } = serviceDesc;
 
 async function main() {
@@ -13,15 +13,15 @@ async function main() {
     try {
         // Variables
         const app = createApp(serviceDesc, express, bodyParser);
-        const rmq = await createRmqConnection(serviceDesc.rmq, amqplib);
+        const rmq = await createRmq(serviceDesc, serviceDesc.rmq, amqplib);
 
         // Http routes
-        app.all('/', (req, res) => {
-            return res.send('Hai, di sini siput. Pesan masuk sejauh ini: ' + pesanMasuk.join(', '));
+        app.all('/', (_, res) => {
+            return res.send('Hai, di sini siput, pesan masuk sejauh ini: ' + pesanMasuk.join(', '));
         });
 
         // Start Listening to rmqEvent
-        await consume(serviceDesc, rmq, rmqEvent, (message) => {
+        await consume(rmq, 'siput', (message) => {
             logger.log("Dapat pesan masuk:", message);
             pesanMasuk.push(message);
         });
